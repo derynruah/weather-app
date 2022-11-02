a07bc1f43756ecb5c32c2240c1c8610f51b2d1fd
Deryn Rushworth <derynrushworth@gmail.com>
2022-11-02 18:18:41 +0000

    edited README and added rules

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/weather app/i);
+  const linkElement = screen.getByText(/weather appq/i);
   expect(linkElement).toBeInTheDocument();
 });
