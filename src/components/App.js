3f02329b1bfc7bc95929ee7d1ee4b9424d317a8b
Deryn Rushworth <derynrushworth@gmail.com>
2022-11-03 17:39:03 +0000

    added forecast summary and summaries

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
+
+function App({ location, forecasts }) {
+  return (
+    <div className="forecast">
+      <LocationDetails city={location.city} country={location.country} />
+
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
+}
+
+App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
