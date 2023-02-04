71fea999cbcb63d142f321c77985cc3817e00e72
Deryn Rushworth <derynrushworth@gmail.com>
2023-02-04 10:44:45 +0000

    updated app and index, added forecastsummaries

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,30 @@ import "../styles/App.css";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
+function App({ forecasts, location }) {
   const { city, country } = location;
   return (
-    <div className="App">
+    <div className="forecast">
       <LocationDetails city={city} country={country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
